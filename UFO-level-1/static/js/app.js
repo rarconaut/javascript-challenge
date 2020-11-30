553d0b410841d08c869cb071a438f4cd064673e6
rarconaut <68246130+rarconaut@users.noreply.github.com>
2020-11-30 13:07:10 -0800

    Regular commit, saving my work.

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,3 +2,31 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+
+// Select the Filter button
+var button = d3.select("#filter-btn");
+
+// Select the date form
+var form = d3.select("#datetime");
+
+// Create event handlers
+button.on("click", runEnter);
+form.on("submit", runEnter);
+
+// Event hanlder function for the form
+function runEnter() {
+    // Prevent page from refreshing
+    d3.event.preventDefault();
+
+    // // Select input element and get raw html node
+    // var inputElement = 
+}
+    // Get the value of the input element
+    var inputValue = form.property("value");
+    
+    console.log(inputValue);
+    console.log(tableData);
+
+    // Filter the data based on user input
+    var filteredData = tableData.filter
+
