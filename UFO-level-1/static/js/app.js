e7186198d5940c5e10dbfb628373afed51b6057f
rarconaut <68246130+rarconaut@users.noreply.github.com>
2020-11-30 14:05:56 -0800

    I'm able to handle the filter button 'click' event (though I get an error for the 'submit' form event??), filter and console.log the tableData based on user input, and I'm about to test appending rows to the html table.

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -7,7 +7,7 @@ var tableData = data;
 var button = d3.select("#filter-btn");
 
 // Select the date form
-var form = d3.select("#datetime");
+var form = d3.select("#form");
 
 // Create event handlers
 button.on("click", runEnter);
@@ -19,14 +19,33 @@ function runEnter() {
     d3.event.preventDefault();
 
     // // Select input element and get raw html node
-    // var inputElement = 
-}
+    var inputElement = d3.select("#datetime");
+
     // Get the value of the input element
-    var inputValue = form.property("value");
+    var inputValue = inputElement.property("value");
     
     console.log(inputValue);
-    console.log(tableData);
+    // console.log(tableData); 
 
     // Filter the data based on user input
-    var filteredData = tableData.filter
+    var filteredData = tableData.filter(ufo => ufo.datetime === inputValue);
+
+    console.log(filteredData);
+
+    // Create a table variable to append the html table
+    var tbody = d3.select("filtered-table");
+
+    // Remove any previous child elements from the table 
+    tbody.html("");
+
+    // Append rows to table
+    filteredData.forEach(ufo => {
+        var row =  tbody.append("tr");
+        Object.entries(ufo).forEach(([key, value]) => {
+            var cell = row.append("td");
+            cell.text(value);
+        });
+    });
+
+};
 
